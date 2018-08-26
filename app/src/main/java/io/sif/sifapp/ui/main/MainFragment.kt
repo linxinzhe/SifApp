fbb16334599ccb36a47e984097240845c8da97a3
linxinzhe <linxinzhechn@qq.com>
2018-08-26 16:40:54 +0800

    feat(ui): layout

--- app/src/main/java/io/sif/sifapp/ui/main/MainFragment.kt
+++ app/src/main/java/io/sif/sifapp/ui/main/MainFragment.kt
@@ -3,9 +3,11 @@ package io.sif.sifapp.ui.main
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import io.sif.sifapp.R
 
 class MainFragment : Fragment() {
@@ -15,10 +17,21 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var photoButton: Button
+    private lateinit var uploadButton: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        var view = inflater.inflate(R.layout.main_fragment, container, false)
+        photoButton = view.findViewById<Button>(R.id.photo_button);
+        photoButton.setOnClickListener {
+            Log.d("photoButton", "trigger photo")
+        }
+        uploadButton = view.findViewById<Button>(R.id.upload_button);
+        uploadButton.setOnClickListener {
+            Log.d("uploadButton", "trigger upload")
+        }
+        return view;
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
